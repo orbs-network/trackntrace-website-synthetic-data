fd0400e2bb8645ec6219fab053fe01987758d8fa
aviram <aviram@orbs.com>
2020-03-08 12:11:16 +0200

    site

---
+++ precache-manifest.8bd7796710c75d51adeee32305983f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbff7cb5a6420e5a587a074d5c98c25d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005f57c7c27ed3d18162",
+    "url": "/static/css/main.3c53a4c9.chunk.css"
+  },
+  {
+    "revision": "8227e79a994282fa233d",
+    "url": "/static/js/2.61c41321.chunk.js"
+  },
+  {
+    "revision": "662c5a7d52c991bfd2ab00183763cd1c",
+    "url": "/static/js/2.61c41321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005f57c7c27ed3d18162",
+    "url": "/static/js/main.384210d4.chunk.js"
+  },
+  {
+    "revision": "c2487f15ff989638a9e4",
+    "url": "/static/js/runtime-main.7dc7e24c.js"
+  }
+]);
