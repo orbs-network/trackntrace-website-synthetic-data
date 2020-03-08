9ead038502a08fbef67c5431f360d754b61561d6
aviram <aviram@orbs.com>
2020-03-08 13:17:56 +0200

    site

---
+++ precache-manifest.448e4b118fd1b386c992d23a71e450a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f70af7f19fd652781b868484028999",
+    "url": "https://orbs-network.github.io/trackntrace-website-synthetic-data/index.html"
+  },
+  {
+    "revision": "48f694c4f5532de1a38a",
+    "url": "https://orbs-network.github.io/trackntrace-website-synthetic-data/static/css/main.e1f5c47c.chunk.css"
+  },
+  {
+    "revision": "b6df38292aaf41e3859b",
+    "url": "https://orbs-network.github.io/trackntrace-website-synthetic-data/static/js/2.2d73bab0.chunk.js"
+  },
+  {
+    "revision": "662c5a7d52c991bfd2ab00183763cd1c",
+    "url": "https://orbs-network.github.io/trackntrace-website-synthetic-data/static/js/2.2d73bab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f694c4f5532de1a38a",
+    "url": "https://orbs-network.github.io/trackntrace-website-synthetic-data/static/js/main.77ed990d.chunk.js"
+  },
+  {
+    "revision": "c3ad3e47953eda5e2853",
+    "url": "https://orbs-network.github.io/trackntrace-website-synthetic-data/static/js/runtime-main.2a1e6db5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bccc64bdfcc5af567dfbbe50c401255c.js"
+  "https://orbs-network.github.io/trackntrace-website-synthetic-data/precache-manifest.448e4b118fd1b386c992d23a71e450a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://orbs-network.github.io/trackntrace-website-synthetic-data/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
